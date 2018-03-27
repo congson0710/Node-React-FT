8ce72501e97f1f0817b60c3736de105389d39b69
congson0710 <congson0710@gmail.com>
2018-03-27 23:16:37 +0700

    connect gg+ api for client ID & secret

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
 
+// client ID: 695430580002-fivsvtkd8ls9f4uc2dbnhi0bv4m221vr.apps.googleusercontent.com
+// client secret: SoF2WoWaLIFDDPNEf9-hOdE6
 
 passport.use(new GoogleStrategy())
 
