593617d5af0295cf0116ad7d85bc33b86d528e01
congson0710 <congson0710@gmail.com>
2018-03-29 21:22:26 +0700

    add mongoose + connect mongoose to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const key = require('./config/key');
+
 require('./services/passport');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+mongoose.connect(key.myMongoDB);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
