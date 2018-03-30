ebc07e9529e6c3ddd2731a6ad62916f0ae0faba4
congson0710 <congson0710@gmail.com>
2018-03-30 19:01:29 +0700

    express use cookie + passport use cookie

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const key = require('./config/key');
-const app = express();
 
 require('./models/Users');
 require('./services/passport');
 
 require('./routes/authRoutes')(app);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [key.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(key.myMongoDB);
 
