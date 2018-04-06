34082d84445a201b34e42638c59e6c149d2eae4a
congson0710 <congson0710@gmail.com>
2018-04-07 00:10:30 +0700

    set up redux, react-redux, react-router-dom

---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case value:
+
+    //     break;
+
+    default:
+      return state;
+  }
+};
