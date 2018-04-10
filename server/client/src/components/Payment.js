e20e2c65f9caf0c2703e35f6e87cd129b89b7f58
congson0710 <congson0710@gmail.com>
2018-04-10 23:25:23 +0700

    update add credit btn UI

--- server/client/src/components/Payment.js
+++ server/client/src/components/Payment.js
@@ -5,10 +5,14 @@ class Payments extends React.Component {
   render() {
     return (
       <StripeCheckout
+        name="eMailee"
+        description="$5 for 5 emails credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">ADD CREDITS</button>
+      </StripeCheckout>
     );
   }
 }
