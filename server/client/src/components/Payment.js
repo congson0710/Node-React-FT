dfcf4ec5d89f5342bcc2448a768a61ba3843b2f5
congson0710 <congson0710@gmail.com>
2018-04-11 21:46:07 +0700

    call handleToken when receive back token from stripe

--- server/client/src/components/Payment.js
+++ server/client/src/components/Payment.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as action from '../actions';
 
 class Payments extends React.Component {
   render() {
@@ -8,7 +10,7 @@ class Payments extends React.Component {
         name="eMailee"
         description="$5 for 5 emails credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">ADD CREDITS</button>
@@ -17,4 +19,4 @@ class Payments extends React.Component {
   }
 }
 
-export default Payments;
+export default connect(null, action)(Payments);
