dd2b33511de69f45fdf3679d120d39a03b6a54c0
congson0710 <congson0710@gmail.com>
2018-04-16 22:23:25 +0700

    redirect on submit

--- server/client/src/components/surveys/SurveyFormPreview.js
+++ server/client/src/components/surveys/SurveyFormPreview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { withRouter } from 'react-router-dom';
 import formField from './formField';
+import * as actions from '../../actions';
 
 const Preview = props => {
-  const { formValues } = props;
+  const { formValues, submitSurvey, history } = props;
   const reviewFields = _.map(formField, field => {
     return (
       <div key={field.name}>
@@ -15,10 +17,17 @@ const Preview = props => {
   });
   return (
     <div>
-      <h1>hi there</h1>
+      <h1>Confirm your entries</h1>
       {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={props.onCancel}>
         Back
+      </button>
+      <button
+        className="green btn-flat right"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -28,4 +37,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(Preview);
+export default connect(mapStateToProps, actions)(withRouter(Preview));
--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormPreview from './SurveyFormPreview';
 
@@ -31,4 +32,6 @@ class SurveyNew extends React.Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm',
+})(SurveyNew);
