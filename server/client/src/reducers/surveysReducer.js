f06c3dc810f58a0c1a63ddf1dc2f17ac87c14084
congson0710 <congson0710@gmail.com>
2018-05-19 15:26:18 +0700

    fetch survey action + reducer

---
+++ server/client/src/reducers/surveysReducer.js
@@ -0,0 +1,15 @@
+import { FETCH_SURVEYS } from '../actions/types';
+
+const surveysReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_SURVEYS:
+      return {
+        ...action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default surveysReducer;
